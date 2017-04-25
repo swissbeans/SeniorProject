7e7f529cdb55ace32eb6c969b8afe60bb41b03cd
Carole Schroeder <swissbeans@gmail.com>
2017-04-25 18:13:47 -0400

    added json files and fixed loadsave to open files

--- loadsave.lua
+++ loadsave.lua
@@ -37,7 +37,7 @@ function M.loadTable(filename, location)
     if location and (not _validLocations[location]) then
      error("Attempted to load a table from an invalid location", 2)
     elseif not location then
-      location = _defaultLocation
+      location = "/"
     end
     local path = system.pathForFile( filename, location)
     local contents = ""
